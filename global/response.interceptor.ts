2fd757841bc31e6b0396d646cbe0ac4de882b217
huangwenqiang <974879409@qq.com>
2023-04-21 00:58:39 +0800

    update

--- global/response.interceptor.ts
+++ global/response.interceptor.ts
@@ -20,7 +20,7 @@ export class GlobalResponseInterceptor<T> implements NestInterceptor {
         const path = request.path;
 
         const chain: string = (request.body.UserDTO || request.body.BrandDTO)?.chain || randomUUID();
-        this.LogRemote.log(ENUM_LOG.TRACE, path, chain); // async
+        this.LogRemote.log(ENUM_LOG.TRACE, `${request.path}@${request.method}`, chain); // async
         const start = Date.now();
 
         return next.handle().pipe(
@@ -30,7 +30,7 @@ export class GlobalResponseInterceptor<T> implements NestInterceptor {
                     data: data ?? null,
                     message: "成功",
                 };
-                this.LogRemote.log(ENUM_LOG.ALL, path, chain, { time: Date.now() - start }); // async
+                this.LogRemote.log(ENUM_LOG.ALL, `${request.path}@${request.method}`, chain, { time: Date.now() - start }); // async
                 return response;
             })
         );
